c7eb4ff50c1f5795edb74e7643b0c875eb9676c5
Mike Eling <mike.eling97@gmail.com>
2020-12-22 21:56:20 +0100

    :tada: Initial commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 // Deps scoped imports.
 import React from "react";
 import { makeStyles } from "@material-ui/core";
-import { useLittera } from "react-littera";
 import cx from "classnames";
 
 // Project scoped imports.
+import logo from 'assets/logo.png'
+import FontAwesome from 'components/FontAwesome'
+
+// Component scoped imports.
 import styles from "./styles";
-import translations from "./trans";
 
 /**
  * Home component
@@ -15,15 +17,39 @@ import translations from "./trans";
  * @author Mike Eling <mike.eling97@gmail.com>
  */
 const Home = (props: ComponentProps) => {
-    const translated = useLittera(translations);
     const classes = useStyles();
 
+    const handleNavigation = (url: string) => () => {
+        window.open(url);
+    }
+
     return <div className={cx(classes.root, props.className)} style={props.style}>
-        <h4 className={classes.h4}>{translated.title}</h4>
+        <div className={classes.contentContainer}>
+            <div className={classes.contentWrapper}>
+                <img alt="logo" src={logo} className={classes.logo} />
+                <div>
+                    <h1 style={{ textTransform: "uppercase", marginBottom: "5px", fontSize: "48px", lineHeight: "45px" }}>Jesteśmy <br />w trakcie przebudowy</h1>
+                    <p style={{ opacity: 0.45 }}>Nasza witryna już w krótce będzie dla was dostępna.<br />Zapraszamy do śledzenia nas w mediach społecznościowych.</p>
+
+                    <br /><br />
+
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start", opacity: 0.4 }}>
+                        <FontAwesome iconProps={{ onClick: handleNavigation("https://fb.karutek.pl/") }} className={classes.socialIcon} >fab fa-facebook</FontAwesome>
+                        <FontAwesome iconProps={{ onClick: handleNavigation("https://ig.karutek.pl/") }} className={classes.socialIcon} >fab fa-instagram</FontAwesome>
+                        <FontAwesome iconProps={{ onClick: handleNavigation("mailto://contact@karutek.pl") }} className={classes.socialIcon} >fas fa-envelope</FontAwesome>
+                    </div>
+                </div>
+                <div style={{ height: "15vh" }}></div>
+            </div>
+        </div>
+        <div className={classes.imageWrapper}>
+            <img alt="thumbnail" src={`https://source.unsplash.com/0J_W8DQzDas/${window.innerWidth / 2}x${window.innerHeight}`} className={classes.image} />
+        </div>
     </div>
 }
 
 // Creates a hook for generating classnames.
+// @ts-ignore
 const useStyles = makeStyles(styles);
 
 // Props the component accepts.
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,10 +3,55 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ((theme: any) => ({
     root: {
-        // Container styles...
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between"
     },
     h4: {
         // Head text styles...
         color: theme.palette.primary
+    },
+    imageWrapper: {
+        [theme.breakpoints.down("md")]: {
+            display: "none"
+        },
+        width: "50%", height: "100%", overflow: "hidden", position: "relative"
+    },
+    contentWrapper: {
+        width: "70%", margin: "0 auto", height: "100%", display: "flex", alignItems: "flex-start", justifyContent: "space-between", flexDirection: "column",
+        [theme.breakpoints.down('md')]: {
+            width: "95%"
+        }
+    },
+    contentContainer: {
+        [theme.breakpoints.down('md')]: {
+            width: "100%"
+        },
+        width: "50%", height: "100%"
+    },
+    image: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "auto",
+        height: "100%"
+    },
+    logo: {
+        width: "250px",
+        height: "auto",
+        [theme.breakpoints.down('md')]: {
+            width: "185px"
+        }
+    },
+    socialIcon: {
+        margin: "0 8px",
+        opacity: 0.8,
+        transition: "opacity 255ms ease",
+        cursor: "pointer",
+        "&:hover": {
+            opacity: 1
+        }
     }
 }));
